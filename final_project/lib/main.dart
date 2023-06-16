e563896840c10b044b1b47510c30810c586b2ab3
Ted Bjurlin <teddybjurlin@gmail.com>
2023-06-16 16:17:49 -0500

    updated group page tests

--- final_project/lib/main.dart
+++ final_project/lib/main.dart
@@ -42,7 +42,7 @@ class MyApp extends StatelessWidget {
         routes: {
           '/groupsPage': (context) => const GroupPage(title: 'List of Groups'),
           '/masterCalendarPage': (context) => const MasterPage(),
-          'loginPage': (context) => const LoginScreen(),
+          '/loginPage': (context) => const LoginScreen(),
         });
   }
 }
--- final_project/test/pages/group_page_test.dart
+++ final_project/test/pages/group_page_test.dart
@@ -13,13 +13,70 @@ import 'package:final_project/objects/app_state.dart';
 import 'group_page_test.mocks.dart';
 
 void main() {
-  testWidgets('StartPage has appBar and page title', (tester) async {
-    await tester.pumpWidget(const MaterialApp(home: GroupPage()));
+  testWidgets('GroupPage has appBar title', (tester) async {
+    MockNavigatorObserver mockObserver = MockNavigatorObserver();
+    MockFirebaseAuth auth = MockFirebaseAuth(signedIn: true);
+    FakeFirebaseFirestore firestore = FakeFirebaseFirestore();
 
-    final appBarTitleFinder = find.text('Action Page');
-    final pageTitleFinder = find.text('Lake Nixon Admin');
+    await tester.pumpWidget(MaterialApp(
+      home: ChangeNotifierProvider(
+          create: ((context) => AppState(firestore, auth)),
+          child: const GroupPage(title: 'List of Groups')),
+      navigatorObservers: [mockObserver],
+    ));
+
+    final appBarTitleFinder = find.text('List of Groups');
 
     expect(appBarTitleFinder, findsOneWidget);
-    expect(pageTitleFinder, findsOneWidget);
+  });
+  testWidgets('GroupPage displays groups', (tester) async {
+    MockNavigatorObserver mockObserver = MockNavigatorObserver();
+    MockFirebaseAuth auth = MockFirebaseAuth(signedIn: true);
+    FakeFirebaseFirestore firestore = FakeFirebaseFirestore();
+
+    await firestore
+        .collection('groups')
+        .doc('Test Group')
+        .set({"age": 100, "color": "Color(0xff000000)", "name": "Test Group"});
+
+    await firestore.collection('groups').doc('Test Group 2').set(
+        {"age": 100, "color": "Color(0xff000000)", "name": "Test Group 2"});
+
+    await tester.pumpWidget(MaterialApp(
+      home: ChangeNotifierProvider(
+          create: ((context) => AppState(firestore, auth)),
+          child: const GroupPage(title: 'List of Groups')),
+      navigatorObservers: [mockObserver],
+    ));
+
+    await tester.pump();
+
+    expect(find.text('Test Group'), findsOneWidget);
+    expect(find.text('Test Group 2'), findsOneWidget);
+  });
+  testWidgets('GroupPage displays abbrev avatars', (tester) async {
+    MockNavigatorObserver mockObserver = MockNavigatorObserver();
+    MockFirebaseAuth auth = MockFirebaseAuth(signedIn: true);
+    FakeFirebaseFirestore firestore = FakeFirebaseFirestore();
+
+    await firestore
+        .collection('groups')
+        .doc('Test Group')
+        .set({"age": 100, "color": "Color(0xff000000)", "name": "Test Group"});
+
+    await firestore.collection('groups').doc('Test Group 2').set(
+        {"age": 100, "color": "Color(0xff000000)", "name": "Test Group 2"});
+
+    await tester.pumpWidget(MaterialApp(
+      home: ChangeNotifierProvider(
+          create: ((context) => AppState(firestore, auth)),
+          child: const GroupPage(title: 'List of Groups')),
+      navigatorObservers: [mockObserver],
+    ));
+
+    await tester.pump();
+
+    expect(find.text('T'), findsNWidgets(2));
+    expect(find.byType(CircleAvatar), findsNWidgets(2));
   });
 }
