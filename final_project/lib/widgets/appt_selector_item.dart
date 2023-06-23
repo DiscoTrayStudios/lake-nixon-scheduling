6f80e56a657819218e4b99aa52df4b1d25ce8dca
Ted Bjurlin <teddybjurlin@gmail.com>
2023-06-23 17:16:47 -0500

    Added doc comments to appt_selector_item.dart

--- final_project/lib/widgets/appt_selector_item.dart
+++ final_project/lib/widgets/appt_selector_item.dart
@@ -3,14 +3,21 @@ import 'package:final_project/objects/lake_appointment.dart';
 import 'package:final_project/pages/appointment_editor.dart';
 import 'package:flutter/material.dart';
 
+/// A widget used to display an appointment in the appointment selector screen.
 class ApptSelectorItem extends StatelessWidget {
+  /// A widget used to display an appointment in the appointment selector screen.
+  ///
+  /// Tapping on this will send the user to the appointment editor to edit
+  /// [this.appointment]
   const ApptSelectorItem(this.appointment, this.dataSource, this.selectedDate,
       {super.key});
 
+  /// The appointment that the widget displays.
   final LakeAppointment appointment;
 
   final AppointmentDataSource dataSource;
 
+  /// The day that the user selected on the calendar.
   final DateTime selectedDate;
 
   @override
