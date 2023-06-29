1eb006e0b7332d85ed527c5c48fef47ca873a2ed
Ted Bjurlin <teddybjurlin@gmail.com>
2023-06-29 15:03:23 -0500

    added nunito, itim, and comic neue fonts

--- lib/objects/theme.dart
+++ lib/objects/theme.dart
@@ -6,7 +6,7 @@ ThemeData lakeNixonTheme() {
       colorScheme: lakeNixonColorScheme(),
 
       // Define the default font family.
-      fontFamily: 'Fruit',
+      fontFamily: 'Itim',
 
       // Define the default `TextTheme`. Use this to specify the default
       // text styling for headlines, titles, bodies of text, and more.
@@ -24,32 +24,32 @@ TextTheme lakeNixonTextTheme() {
   return const TextTheme(
     bodyLarge: TextStyle(
         fontSize: 26,
-        fontWeight: FontWeight.w300,
+        fontWeight: FontWeight.w900,
         wordSpacing: 0.15,
         color: Color.fromRGBO(81, 146, 78, 1)),
     bodyMedium: TextStyle(
         fontSize: 18,
-        fontWeight: FontWeight.w300,
+        fontWeight: FontWeight.w900,
         wordSpacing: 0.25,
         color: Color.fromRGBO(137, 116, 73, 1)),
     bodySmall: TextStyle(
         fontSize: 12,
-        fontWeight: FontWeight.w300,
+        fontWeight: FontWeight.w900,
         wordSpacing: 0.4,
         color: Color.fromRGBO(137, 116, 73, 1)),
     titleLarge: TextStyle(
         fontSize: 24,
-        fontWeight: FontWeight.w400,
+        fontWeight: FontWeight.w900,
         wordSpacing: 0.4,
         color: Colors.white),
     titleMedium: TextStyle(
         fontSize: 20,
-        fontWeight: FontWeight.w300,
+        fontWeight: FontWeight.w900,
         wordSpacing: 0.4,
         color: Colors.white),
     displaySmall: TextStyle(
         fontSize: 42,
-        fontWeight: FontWeight.w400,
+        fontWeight: FontWeight.w900,
         color: Color.fromRGBO(81, 146, 78, 1)),
   );
 }
@@ -62,19 +62,19 @@ extension LakeNixonColorScheme on ColorScheme {
 
 extension LakeNixonTextTheme on TextTheme {
   TextStyle get appBarTitle => const TextStyle(
-      fontSize: 32, fontWeight: FontWeight.w400, color: Colors.white);
+      fontSize: 32, fontWeight: FontWeight.w900, color: Colors.white);
   TextStyle get appBarFilter => const TextStyle(
-      fontSize: 18, fontWeight: FontWeight.w300, color: Colors.white);
+      fontSize: 18, fontWeight: FontWeight.w900, color: Colors.white);
   TextStyle get smallButton => const TextStyle(
-      fontSize: 32, fontWeight: FontWeight.w300, color: Colors.white);
+      fontSize: 32, fontWeight: FontWeight.w900, color: Colors.white);
   TextStyle get mediumButton => const TextStyle(
       fontSize: 20,
-      fontWeight: FontWeight.w400,
+      fontWeight: FontWeight.w900,
       color: Color.fromRGBO(81, 146, 78, 1));
   TextStyle get largeButton => const TextStyle(
-      fontSize: 42, fontWeight: FontWeight.w400, color: Colors.white);
+      fontSize: 42, fontWeight: FontWeight.w900, color: Colors.white);
   TextStyle get pageHeader => const TextStyle(
       fontSize: 32,
-      fontWeight: FontWeight.w400,
+      fontWeight: FontWeight.w900,
       color: Color.fromRGBO(137, 116, 73, 1));
 }
