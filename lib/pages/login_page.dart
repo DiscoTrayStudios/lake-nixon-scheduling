5d6d6a208b9b6e9fe3dbe2977716cf4587666ed0
Ted Bjurlin <teddybjurlin@gmail.com>
2023-07-03 17:56:26 -0500

    Updated necessary pages to use flex widgets for tablet layout

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,4 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:final_project/objects/theme.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -36,6 +35,7 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
+    bool isHandset = size.width < 600;
     return Scaffold(
         body: SizedBox(
             width: size.width,
@@ -43,135 +43,176 @@ class _LoginScreenState extends State<LoginScreen> {
             child: SingleChildScrollView(
                 child: Padding(
                     padding: const EdgeInsets.all(10),
-                    child: Column(
+                    child: Flex(
+                      direction: isHandset ? Axis.vertical : Axis.horizontal,
                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
                         Container(
+                            constraints: BoxConstraints(
+                              maxHeight: isHandset
+                                  ? (size.height / 2) - 20
+                                  : size.height - 20,
+                              maxWidth: isHandset
+                                  ? size.width - 20
+                                  : (size.width / 2) - 20,
+                            ),
                             alignment: Alignment.center,
                             padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
                             child: const Image(
                                 image: AssetImage('images/lakenixonlogo.png'))),
-                        Container(
-                            alignment: Alignment.center,
-                            padding: const EdgeInsets.all(10),
-                            child: Text(
-                              'Sign in',
-                              style: Theme.of(context).textTheme.displaySmall,
-                            )),
-                        Container(
-                          padding: const EdgeInsets.all(10),
-                          child: TextField(
-                            controller: emailController,
-                            style: Theme.of(context).textTheme.headlineSmall,
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'E-mail',
-                            ),
-                          ),
-                        ),
-                        Container(
-                          padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-                          child: TextField(
-                            obscureText: true,
-                            controller: passwordController,
-                            style: Theme.of(context).textTheme.headlineSmall,
-                            decoration: const InputDecoration(
-                              border: OutlineInputBorder(),
-                              labelText: 'Password',
-                            ),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            forgotPassword();
-                          },
-                          child: Text('Forgot Password',
-                              style: Theme.of(context).textTheme.mediumButton),
-                        ),
-                        Container(
-                            width: MediaQuery.of(context).size.width,
-                            height: 80,
-                            padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                            child: ElevatedButton(
-                              style: ButtonStyle(
-                                  shape: MaterialStateProperty.all<
-                                          RoundedRectangleBorder>(
-                                      RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(40.0),
-                                  )),
-                                  backgroundColor: MaterialStatePropertyAll<
-                                          Color>(
-                                      Theme.of(context).colorScheme.primary)),
-                              child: Text(
-                                'Login',
-                                style: Theme.of(context).textTheme.largeButton,
-                              ),
-                              onPressed: () async {
-                                try {
-                                  await FirebaseAuth.instance
-                                      .signInWithEmailAndPassword(
-                                          email: emailController.text,
-                                          password: passwordController.text);
-                                } on FirebaseAuthException catch (e) {
-                                  if (e.code == 'user-not-found') {
-                                    Fluttertoast.showToast(
-                                        msg:
-                                            "User not found under that email address",
-                                        toastLength: Toast.LENGTH_LONG,
-                                        gravity: ToastGravity.CENTER,
-                                        timeInSecForIosWeb: 3,
-                                        backgroundColor:
-                                            Theme.of(context).colorScheme.error,
-                                        textColor: Theme.of(context)
-                                            .colorScheme
-                                            .onError,
-                                        fontSize: Theme.of(context)
-                                            .textTheme
-                                            .bodyLarge!
-                                            .fontSize);
-                                    debugPrint('No user found for that email.');
-                                  } else if (e.code == 'wrong-password') {
-                                    // probably should not say something like this for security
-                                    Fluttertoast.showToast(
-                                        msg:
-                                            "Incorrect password for that email",
-                                        toastLength: Toast.LENGTH_LONG,
-                                        gravity: ToastGravity.CENTER,
-                                        timeInSecForIosWeb: 3,
-                                        backgroundColor:
-                                            Theme.of(context).colorScheme.error,
-                                        textColor: Theme.of(context)
-                                            .colorScheme
-                                            .onError,
-                                        fontSize: Theme.of(context)
-                                            .textTheme
-                                            .bodyLarge!
-                                            .fontSize);
-                                    debugPrint(
-                                        'Wrong password provided for that user.');
-                                  }
-                                }
-
-                                login();
-                              },
-                            )),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
+                        Column(
                           children: <Widget>[
-                            Text(
-                              'Don\'t have account?',
-                              style: Theme.of(context).textTheme.bodyMedium,
+                            Container(
+                                alignment: Alignment.center,
+                                padding: const EdgeInsets.all(10),
+                                child: Text(
+                                  'Sign in',
+                                  style:
+                                      Theme.of(context).textTheme.displaySmall,
+                                )),
+                            Container(
+                              padding: const EdgeInsets.all(10),
+                              constraints: BoxConstraints(
+                                maxWidth: isHandset
+                                    ? size.width - 20
+                                    : (size.width / 2) - 20,
+                              ),
+                              child: TextField(
+                                controller: emailController,
+                                style:
+                                    Theme.of(context).textTheme.headlineSmall,
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'E-mail',
+                                ),
+                              ),
+                            ),
+                            Container(
+                              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                              constraints: BoxConstraints(
+                                maxWidth: isHandset
+                                    ? size.width - 20
+                                    : (size.width / 2) - 20,
+                              ),
+                              child: TextField(
+                                obscureText: true,
+                                controller: passwordController,
+                                style:
+                                    Theme.of(context).textTheme.headlineSmall,
+                                decoration: const InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  labelText: 'Password',
+                                ),
+                              ),
                             ),
                             TextButton(
-                              child: Text(
-                                'Sign up',
-                                style: Theme.of(context).textTheme.mediumButton,
-                              ),
-                              onPressed: () async {
-                                await Navigator.pushNamed(
-                                    context, '/signupPage');
+                              onPressed: () {
+                                forgotPassword();
                               },
-                            )
+                              child: Text('Forgot Password',
+                                  style:
+                                      Theme.of(context).textTheme.mediumButton),
+                            ),
+                            Container(
+                                width: isHandset
+                                    ? size.width - 20
+                                    : (size.width / 2) - 20,
+                                height: 80,
+                                padding:
+                                    const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                                child: ElevatedButton(
+                                  style: ButtonStyle(
+                                      shape: MaterialStateProperty.all<
+                                              RoundedRectangleBorder>(
+                                          RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(40.0),
+                                      )),
+                                      backgroundColor:
+                                          MaterialStatePropertyAll<Color>(
+                                              Theme.of(context)
+                                                  .colorScheme
+                                                  .primary)),
+                                  child: Text(
+                                    'Login',
+                                    style:
+                                        Theme.of(context).textTheme.largeButton,
+                                  ),
+                                  onPressed: () async {
+                                    try {
+                                      await FirebaseAuth.instance
+                                          .signInWithEmailAndPassword(
+                                              email: emailController.text,
+                                              password:
+                                                  passwordController.text);
+                                    } on FirebaseAuthException catch (e) {
+                                      if (e.code == 'user-not-found') {
+                                        Fluttertoast.showToast(
+                                            msg:
+                                                "User not found under that email address",
+                                            toastLength: Toast.LENGTH_LONG,
+                                            gravity: ToastGravity.CENTER,
+                                            timeInSecForIosWeb: 3,
+                                            backgroundColor: Theme.of(context)
+                                                .colorScheme
+                                                .error,
+                                            textColor: Theme.of(context)
+                                                .colorScheme
+                                                .onError,
+                                            fontSize: Theme.of(context)
+                                                .textTheme
+                                                .bodyLarge!
+                                                .fontSize);
+                                        debugPrint(
+                                            'No user found for that email.');
+                                      } else if (e.code == 'wrong-password') {
+                                        // probably should not say something like this for security
+                                        Fluttertoast.showToast(
+                                            msg:
+                                                "Incorrect password for that email",
+                                            toastLength: Toast.LENGTH_LONG,
+                                            gravity: ToastGravity.CENTER,
+                                            timeInSecForIosWeb: 3,
+                                            backgroundColor: Theme.of(context)
+                                                .colorScheme
+                                                .error,
+                                            textColor: Theme.of(context)
+                                                .colorScheme
+                                                .onError,
+                                            fontSize: Theme.of(context)
+                                                .textTheme
+                                                .bodyLarge!
+                                                .fontSize);
+                                        debugPrint(
+                                            'Wrong password provided for that user.');
+                                      }
+                                    }
+
+                                    login();
+                                  },
+                                )),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: <Widget>[
+                                Text(
+                                  'Don\'t have account?',
+                                  style: Theme.of(context).textTheme.bodyMedium,
+                                ),
+                                TextButton(
+                                  child: Text(
+                                    'Sign up',
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .mediumButton,
+                                  ),
+                                  onPressed: () async {
+                                    await Navigator.pushNamed(
+                                        context, '/signupPage');
+                                  },
+                                )
+                              ],
+                            ),
                           ],
                         ),
                       ],
