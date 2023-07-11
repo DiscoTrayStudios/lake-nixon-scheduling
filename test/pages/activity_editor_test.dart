b676121b021308f80ce813ebbe4af55714dc087f
Ted Bjurlin <teddybjurlin@gmail.com>
2023-07-11 10:25:52 -0500

    started activity editor tests

--- test/pages/activity_editor_test.dart
+++ test/pages/activity_editor_test.dart
@@ -1,8 +1,5 @@
 import 'package:final_project/objects/app_state.dart';
-import 'package:flutter/material.dart';
 import 'package:mockito/mockito.dart';
-
-import 'activity_editor_test.mocks.dart';
 
 class MockAppState extends Mock implements AppState {}
 
